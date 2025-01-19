92389355f910e837b3926320ef9194f6212d0726
Vortex <72176308+Yanxinwu946@users.noreply.github.com>
2025-01-19 11:52:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,26 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-echo "检测当前的网络接口..."
+echo "检测本机网络接口..."
 ip link show
 
-read -p "输入切换网卡，不输入默认跳过: " user_input
+# 从 ./scripts/stage 文件中定位网卡名称
+current_interface=$(grep -oP '# Get current IP on \K\w+' ./scripts/stage)
 
-if [ -n "$user_input" ]; then
-    echo "正在将网卡名称替换为 $user_input..."
-    find ./scripts -type f ! -name "*.md" -exec sed -i "s/tun0/$user_input/g" {} \;
+# 检查是否找到了网卡名称
+if [ -n "$current_interface" ]; then
+    echo "当前脚本应用的网卡是: $current_interface"
+    
+    # 提示用户输入新的网卡名称
+    read -p "输入切换的网卡（不输入默认跳过）: " user_input
+    
+    # 如果用户输入了新网卡名称，则进行替换
+    if [ -n "$user_input" ]; then
+        echo "正在将网卡名称替换为 $user_input..."
+        find ./scripts -type f ! -name "*.md" -exec sed -i "s/$current_interface/$user_input/g" {} \;
+    fi
+else
+    echo "未在 ./scripts/stage 文件中找到当前网卡名称，无法进行替换。"
 fi
 
 find . -type f ! -name "*.md" -exec chmod +x {} \;
